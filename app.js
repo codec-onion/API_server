14570c1083065c04b5d873a1a250ab042bdc6500
codec-onion <bonnetmatthieu86@gmail.com>
2025-03-10 16:45:18 +0100

    first step

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express")
+// const dotenv = require("dotenv").config()
+// const mongoose = require("mongoose")
+// const userRoutes = require("./routes/user")
+// const sauceRoutes = require("./routes/sauce")
+// const path = require("path")
+
+// mongoose
+//   .connect(`${process.env.MONGODB_URL}`, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("Connexion à MongoDB réussie !"))
+//   .catch((error) => console.log("Connexion à MongoDB échouée !"))
+
+const app = express()
+
+app.use("/coucou", (req, res) => res.send("Coucou"))
+// .use(express.json())
+// .use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*")
+//   res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content, Accept, Content-Type, Authorization")
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+//   next()
+// })
+// .use("/api/auth", userRoutes)
+// .use("/api/sauces", sauceRoutes)
+// .use("/images", express.static(path.join(__dirname, 'images')))
+
+module.exports = app
